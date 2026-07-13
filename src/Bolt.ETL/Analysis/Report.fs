da6b1644ba5cb2fb251deb9d9ad3a31cb784b43b
Radosław Kot <radoslaw.edward.kot@gmail.com>
2026-07-13 18:43:39 +0200

    feat: structured analysis report types

---
+++ src/Bolt.ETL/Analysis/Report.fs
@@ -0,0 +1,31 @@
+namespace Bolt.ETL.Analysis
+
+open System
+
+type ResultTable = {
+    Title: string
+    Headers: string list
+    Rows: string list list
+}
+
+type ResultChart = {
+    Title: string
+    /// Plotly figure JSON ({"data": …, "layout": …}), rendered client-side.
+    PlotlyFigureJson: string
+}
+
+type AnalysisSection = {
+    Id: string
+    Title: string
+    Description: string
+    Charts: ResultChart list
+    Tables: ResultTable list
+}
+
+type AnalysisReport = {
+    Email: string
+    GeneratedAt: DateTimeOffset
+    RideCount: int
+    DateRange: DateTimeOffset * DateTimeOffset
+    Sections: AnalysisSection list
+}
